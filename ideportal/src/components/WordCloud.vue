9b07fdd6bcbb55feacfd7af143ddf2db0d422792
waninko <43968374+waninko@users.noreply.github.com>
2020-07-06 13:15:36 +0200

    ordsky med klikkbare ord

--- ideportal/src/components/WordCloud.vue
+++ ideportal/src/components/WordCloud.vue
@@ -1,24 +1,59 @@
 <template>
     <div>
-        <ul v-for="(category, index) in categories" :key=index>
+        <!-- <ul v-for="(category, index) in categories" :key=index>
             <li @click="selectItem(category)">{{category}}</li>
 
-        </ul>
+        </ul> -->
       
+<vue-word-cloud
+  style="
+    height: 180px;
+    width: 240px;
+    margin-left:42%;
+    padding: 20px;
+  "
+  :words="categories"
+  :color="([, weight]) => weight > 8 ? 'DeepPink' : weight > 4 ? 'RoyalBlue' : 'Indigo'"
+  font-family="Roboto"
+>
+<div slot-scope="{text, weight}">
+     <div :title="weight" class="word" @click="selectItem(text)">
+      {{ text }}
+    </div>
+</div>
+</vue-word-cloud>
+
     </div>
 </template>
 
 <script>
 //ordsky tid. :|
+import VueWordCloud from 'vuewordcloud';
+
+
+//ta vare på antall prosjekter i hver category - flere prosj, større tekst
 export default {
   name: "WordCloud",
+  components: {
+    [VueWordCloud.name]: VueWordCloud
+  },
   data (){
       return {
-          categories:["JS","C#","Vue<3","SQL","React","HTML/CSS"],
-      }
+        // categories:["JS","C#","Vue<3","SQL","React","HTML/CSS"],
+          categories:[
+              ["JS",5],
+              ["C#", 9],
+              ["HTML/CSS", 5],
+              ["Vue", 6],
+              ["SQL", 4]
+              ],
+          randomNr: Math.floor(Math.random() * 11)
+          
+  }
   },
   methods: {
       selectItem(clickedCat){
+          console.log(clickedCat, " select kjører")
           this.$emit('cloudToHome', clickedCat)
       }
   },
@@ -26,3 +61,18 @@ export default {
   
 };
 </script>
+
+
+<style scoped>
+.cloud{
+
+}
+
+.word{
+cursor: pointer; 
+}
+.word:hover{
+    /* border: 3px dotted #3a7999; */
+	color: orange;
+ }
+</style>
